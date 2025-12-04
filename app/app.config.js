95f36eb59fb02fee471e70aad0620ddd436aec10
Yoones Khoshghadam <yooneskh@gmail.com>
2025-12-04 09:25:10 +0300

    fix: condense table

--- app/app.config.js
+++ app/app.config.js
@@ -2,10 +2,18 @@
 
 export default defineAppConfig({
   ui: {
+
     card: {
       slots: {
         body: 'p-3 sm:p-3',
       },
     },
+
+    table: {
+      slots: {
+        td: 'p-3',
+      },
+    },
+
   },
 });
